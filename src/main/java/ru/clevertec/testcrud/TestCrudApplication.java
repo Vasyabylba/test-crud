dbc19056c005d92b94699c2672730261c0f80419
Vasiliy Yalovchik <vasyabylba@gmail.com>
2024-09-15 18:37:00 +0300

    init commit

---
+++ src/main/java/ru/clevertec/testcrud/TestCrudApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.testcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCrudApplication.class, args);
+	}
+
+}
